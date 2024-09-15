3b815e561939b64c836c02ce3d12d1a3e8d86591
funcdfs <funcdfs@gmail.com>
2024-09-15 14:42:54 +0800

    upd init

---
+++ lua/plugins.lua
@@ -0,0 +1,106 @@
+-- Install Lazy.nvim automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- After installation, run `checkhealth lazy` to see if everything goes right
+-- Hints:
+--     build: It will be executed when a plugin is installed or updated
+--     config: It will be executed when the plugin loads
+--     event: Lazy-load on event
+--     dependencies: table
+--                   A list of plugin names or plugin specs that should be loaded when the plugin loads.
+--                   Dependencies are always lazy-loaded unless specified otherwise.
+--     ft: Lazy-load on filetype
+--     cmd: Lazy-load on command
+--     init: Functions are always executed during startup
+--     branch: string?
+--             Branch of the repository
+--     main: string?
+--           Specify the main module to use for config() or opts()
+--           , in case it can not be determined automatically.
+--     keys: string? | string[] | LazyKeysSpec table
+--           Lazy-load on key mapping
+--     opts: The table will be passed to the require(...).setup(opts)
+require("lazy").setup({
+	-- LSP manager
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	-- Add hooks to LSP to support Linter && Formatter
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			-- Note:
+			--     the default search path for `require` is ~/.config/nvim/lua
+			--     use a `.` as a path seperator
+			--     the suffix `.lua` is not needed
+			require("config.mason-null-ls")
+		end,
+	},
+	-- Vscode-like pictograms
+	{
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	-- Code snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+	},
+	-- Colorscheme
+	"tanvirtin/monokai.nvim",
+	-- Autopairs: [], (), "", '', etc
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("config.nvim-autopairs")
+		end,
+	},
+	-- Show indentation and blankline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("config.indent-blankline")
+		end,
+	},
+	-- Better terminal integration
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("config.toggleterm")
+		end,
+	},
+})
